9bd4d5ebea6a5fe2eef8785408679c40a9bb1299
nishantsarda-mcw <nishant.sarda@multicorewareinc.com>
2024-09-09 14:03:49 +0530

    this is the transpose changes

--- tensorflow/lite/kernels/stablehlo_dot_general.cc
+++ tensorflow/lite/kernels/stablehlo_dot_general.cc
@@ -21,6 +21,7 @@ limitations under the License.
 #include "tensorflow/lite/core/c/builtin_op_data.h"
 #include "tensorflow/lite/core/c/c_api_types.h"
 #include "tensorflow/lite/core/c/common.h"
+#include "tensorflow/lite/kernels/internal/optimized/optimized_ops.h"
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
 #include "tensorflow/lite/kernels/tensor_slice_util.h"
@@ -32,14 +33,22 @@ namespace stablehlo_dot_general {
 namespace {
 
 static constexpr int kMaxDims = 6;
+static constexpr int kMaxTemporaryTensors = 2;
 
 struct DotGeneralData {
  public:
   enum { kLhsTensor, kRhsTensor };
   enum { kOutputTensor };
-
+  // The index of the temporary tensors
+  int scratch_tensor_index;
   std::vector<int64_t> lhs_result_dims;
   std::vector<int64_t> rhs_result_dims;
+  std::vector<int64_t> newaxes_lhs;
+  std::vector<int64_t> newaxes_rhs;
+  int64_t newshape_lhs[3];
+  int64_t newshape_rhs[3];
+  bool do_lhs_reshape;
+  bool do_rhs_reshape;
 };
 
 using TfLiteIntArrayUniquePtr =
@@ -217,18 +226,194 @@ static TfLiteIntArrayUniquePtr GetResultShape(
   return result;
 }
 
+TfLiteStatus PrepareTemporaries(TfLiteContext* context, TfLiteNode* node,
+                                const TfLiteStablehloDotGeneralParams* params,
+                                const TfLiteTensor* lhs,
+                                const TfLiteTensor* rhs, TfLiteTensor* output) {
+  DotGeneralData& opdata = *reinterpret_cast<DotGeneralData*>(node->user_data);
+
+  context->AddTensors(context, kMaxTemporaryTensors,
+                      &opdata.scratch_tensor_index);
+  TfLiteIntArrayFree(node->temporaries);
+  node->temporaries = TfLiteIntArrayCreate(kMaxTemporaryTensors);
+
+  const int lhs_rank = NumDimensions(lhs);
+  const int rhs_rank = NumDimensions(rhs);
+  const int output_rank = NumDimensions(output);
+  const int lhsb_size = params->num_lhs_batching_dimensions;
+  const int rhsb_size = params->num_rhs_batching_dimensions;
+  const int lhsc_size = params->num_lhs_contracting_dimensions;
+  const int rhsc_size = params->num_rhs_contracting_dimensions;
+  const int num_lhs_result_dims = opdata.lhs_result_dims.size();
+  const int num_rhs_result_dims = opdata.rhs_result_dims.size();
+  // prepare transpose tensors - putting batching dims at the start and
+  // contracting dims at the end
+  for (int i = 0; i < lhsb_size; ++i) {
+    opdata.newaxes_lhs.push_back(params->lhs_batching_dimensions[i]);
+  }
+  for (int i = 0; i < num_lhs_result_dims; ++i) {
+    opdata.newaxes_lhs.push_back(opdata.lhs_result_dims[i]);
+  }
+  for (int i = 0; i < lhsc_size; ++i) {
+    opdata.newaxes_lhs.push_back(params->lhs_contracting_dimensions[i]);
+  }
+  for (int i = 0; i < rhsb_size; ++i) {
+    opdata.newaxes_rhs.push_back(params->rhs_batching_dimensions[i]);
+  }
+  for (int i = 0; i < num_rhs_result_dims; ++i) {
+    opdata.newaxes_rhs.push_back(opdata.rhs_result_dims[i]);
+  }
+  for (int i = 0; i < rhsc_size; ++i) {
+    opdata.newaxes_rhs.push_back(params->rhs_contracting_dimensions[i]);
+  }
+  // check if lhs reshape is required. If lhs rank is 3 and lhsb_size == 1 and
+  // lhsc_size == 1 then no reshape is required
+  if (lhs_rank == 3 && lhsb_size == 1 && lhsc_size == 1) {
+    opdata.do_lhs_reshape = false;
+  } else {
+    int64_t dim = 1;
+    if (lhsb_size == 0) {
+      opdata.newshape_lhs[0] = 1;
+    } else {
+      for (int i = 0; i < lhsb_size; ++i) {
+        dim *= lhs->dims->data[params->lhs_batching_dimensions[i]];
+      }
+      opdata.newshape_lhs[0] = dim;
+    }
+    dim = 1;
+    for (int i = 0; i < num_lhs_result_dims; ++i) {
+      dim *= lhs->dims->data[opdata.lhs_result_dims[i]];
+    }
+    opdata.newshape_lhs[1] = dim;
+    dim = 1;
+    for (int i = 0; i < lhsc_size; ++i) {
+      dim *= lhs->dims->data[params->lhs_contracting_dimensions[i]];
+    }
+    opdata.newshape_lhs[2] = dim;
+    opdata.do_lhs_reshape = true;
+  }
+  // check if rhs reshape is required
+  if (rhs_rank == 3 && rhsb_size == 1 && rhsc_size == 1) {
+    opdata.do_rhs_reshape = false;
+  } else {
+    int64_t dim = 1;
+    if (rhsb_size == 0) {
+      opdata.newshape_rhs[0] = 1;
+    } else {
+      for (int i = 0; i < rhsb_size; ++i) {
+        dim *= rhs->dims->data[params->rhs_batching_dimensions[i]];
+      }
+      opdata.newshape_rhs[0] = dim;
+    }
+    dim = 1;
+    for (int i = 0; i < num_rhs_result_dims; ++i) {
+      dim *= rhs->dims->data[opdata.rhs_result_dims[i]];
+    }
+    opdata.newshape_rhs[1] = dim;
+    dim = 1;
+    for (int i = 0; i < rhsc_size; ++i) {
+      dim *= rhs->dims->data[params->rhs_contracting_dimensions[i]];
+    }
+    opdata.newshape_rhs[2] = dim;
+    opdata.do_rhs_reshape = true;
+  }
+  // lhs_transpose prepare
+  node->temporaries->data[0] = opdata.scratch_tensor_index;
+  TfLiteTensor* lhs_transpose;
+  TF_LITE_ENSURE_OK(
+      context, GetTemporarySafe(context, node, /*index=*/0, &lhs_transpose));
+  TfLiteIntArray* lhs_transpose_shape = TfLiteIntArrayCreate(lhs_rank);
+  for (int i = 0; i < lhs_rank; ++i) {
+    lhs_transpose_shape->data[i] = lhs->dims->data[opdata.newaxes_lhs[i]];
+  }
+  lhs_transpose->type = lhs->type;
+  lhs_transpose->allocation_type = kTfLiteArenaRw;
+  TF_LITE_ENSURE_OK(context, context->ResizeTensor(context, lhs_transpose,
+                                                   lhs_transpose_shape));
+  // lhs reshape - to 3D tensor
+  if (opdata.do_lhs_reshape) {
+    const int lhs_reshape_size = 3;
+    TfLiteIntArray* lhs_reshape = TfLiteIntArrayCreate(lhs_reshape_size);
+    for (int i = 0; i < lhs_reshape_size; ++i) {
+      lhs_reshape->data[i] = opdata.newshape_lhs[i];
+    }
+    TF_LITE_ENSURE_OK(
+        context, context->ResizeTensor(context, lhs_transpose, lhs_reshape));
+  }
+  // rhs_transpose prepare
+  node->temporaries->data[1] = opdata.scratch_tensor_index + 1;
+  TfLiteTensor* rhs_transpose;
+  TF_LITE_ENSURE_OK(
+      context, GetTemporarySafe(context, node, /*index=*/1, &rhs_transpose));
+  TfLiteIntArray* rhs_transpose_shape = TfLiteIntArrayCreate(rhs_rank);
+  for (int i = 0; i < rhs_rank; ++i) {
+    rhs_transpose_shape->data[i] = rhs->dims->data[opdata.newaxes_rhs[i]];
+  }
+  rhs_transpose->type = rhs->type;
+  rhs_transpose->allocation_type = kTfLiteArenaRw;
+  TF_LITE_ENSURE_OK(context, context->ResizeTensor(context, rhs_transpose,
+                                                   rhs_transpose_shape));
+  // rhs reshape - to 3D tensor
+  if (opdata.do_rhs_reshape) {
+    const int rhs_reshape_size = 3;
+    TfLiteIntArray* rhs_reshape = TfLiteIntArrayCreate(rhs_reshape_size);
+    for (int i = 0; i < rhs_reshape_size; ++i) {
+      rhs_reshape->data[i] = opdata.newshape_rhs[i];
+    }
+    TF_LITE_ENSURE_OK(
+        context, context->ResizeTensor(context, rhs_transpose, rhs_reshape));
+  }
+  return kTfLiteOk;
+}
+
 template <typename DataType>
 TfLiteStatus EvalImpl(TfLiteContext* context, TfLiteNode* node,
                       const TfLiteTensor* lhs, const TfLiteTensor* rhs,
                       TfLiteTensor* output) {
-  const DataType* lhs_data = GetTensorData<DataType>(lhs);
-  const DataType* rhs_data = GetTensorData<DataType>(rhs);
+  DotGeneralData& opdata = *reinterpret_cast<DotGeneralData*>(node->user_data);
+
+  const int lhs_rank = NumDimensions(lhs);
+  const int rhs_rank = NumDimensions(rhs);
+  // lhs transpose
+  RuntimeShape lhs_transposed_shape(GetTensorShape(lhs));
+  RuntimeShape lhs_shape(GetTensorShape(lhs));
+  for (int i = 0; i < lhs_shape.DimensionsCount(); ++i) {
+    lhs_transposed_shape.SetDim(i, lhs_shape.Dims(opdata.newaxes_lhs[i]));
+  }
+  TransposeParams lhs_params;
+  lhs_params.perm_count = lhs_rank;
+  for (int i = 0; i < lhs_rank; ++i) {
+    lhs_params.perm[i] = opdata.newaxes_lhs[i];
+  }
+  TfLiteTensor* lhs_transpose = GetTemporary(context, node, 0);
+  optimized_ops::Transpose(lhs_params, lhs_shape, GetTensorData<DataType>(lhs),
+                           lhs_transposed_shape,
+                           GetTensorData<DataType>(lhs_transpose));
+  // rhs transpose
+  RuntimeShape rhs_transposed_shape(GetTensorShape(rhs));
+  RuntimeShape rhs_shape(GetTensorShape(rhs));
+  for (int i = 0; i < rhs_shape.DimensionsCount(); ++i) {
+    rhs_transposed_shape.SetDim(i, rhs_shape.Dims(opdata.newaxes_rhs[i]));
+  }
+  TransposeParams rhs_params;
+  rhs_params.perm_count = rhs_rank;
+  for (int i = 0; i < rhs_rank; ++i) {
+    rhs_params.perm[i] = opdata.newaxes_rhs[i];
+  }
+  TfLiteTensor* rhs_transpose = GetTemporary(context, node, 1);
+  optimized_ops::Transpose(rhs_params, rhs_shape, GetTensorData<DataType>(rhs),
+                           rhs_transposed_shape,
+                           GetTensorData<DataType>(rhs_transpose));
+
+  // matrix multiplication using eigen
+  const DataType* lhs_data = GetTensorData<DataType>(lhs_transpose);
+  const DataType* rhs_data = GetTensorData<DataType>(rhs_transpose);
   DataType* output_data = GetTensorData<DataType>(output);
 
-  const int batch_size = lhs->dims->data[0];
-  const int n = lhs->dims->data[1];
-  const int p = lhs->dims->data[2];
-  const int m = rhs->dims->data[1];
+  const int batch_size = lhs_transpose->dims->data[0];
+  const int n = lhs_transpose->dims->data[1];
+  const int p = lhs_transpose->dims->data[2];
+  const int m = rhs_transpose->dims->data[1];
   const int output_batch_size = n * m;
   const int lhs_batch_size = n * p;
   const int rhs_batch_size = m * p;
@@ -294,6 +479,10 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   TfLiteIntArrayUniquePtr result_shape = GetResultShape(node, params, lhs, rhs);
   TF_LITE_ENSURE_STATUS(
       context->ResizeTensor(context, output, result_shape.release()));
+  // Prepare temporary tensors for lhs and rhs transpose
+  TF_LITE_ENSURE_OK(
+      context, PrepareTemporaries(context, node, params, lhs, rhs, output));
+
   return TfLiteStatus::kTfLiteOk;
 }
 
--- tensorflow/lite/kernels/stablehlo_dot_general_test.cc
+++ tensorflow/lite/kernels/stablehlo_dot_general_test.cc
@@ -28,6 +28,7 @@ limitations under the License.
 namespace tflite {
 namespace {
 
+using testing::ElementsAreArray;
 using testing::FloatEq;
 using testing::FloatNear;
 using testing::Pointwise;
@@ -85,6 +86,33 @@ class StablehloDotGeneralOpModel : public SingleOpModel {
   int output_;
 };
 
+TEST(StablehloDotGeneralModelTest, DotGeneralInt32_ScalarOutput) {
+  TfLiteStablehloDotGeneralParams params = {
+      {0},     // lhs_batching_dimensions;
+      1,       // num_lhs_batching_dimensions
+      {0},     // rhs_batching_dimensions;
+      1,       // num_rhs_batching_dimensions
+      {2, 1},  // lhs_contracting_dimensions;
+      2,       // num_lhs_contracting_dimensions
+      {1, 2},  // rhs_contracting_dimensions;
+      2,       // num_rhs_contracting_dimensions
+      2,       // num_precision_configs
+      {tflite::StablehloPrecisionConfig::StablehloPrecisionConfig_DEFAULT,
+       tflite::StablehloPrecisionConfig::
+           StablehloPrecisionConfig_DEFAULT}  // precision config;
+  };
+  StablehloDotGeneralOpModel model({TensorType_INT32, {1, 3, 4}},
+                                   {TensorType_INT32, {1, 4, 3}},
+                                   {TensorType_INT32, {}}, params);
+
+  model.SetInputs<int32_t>({2, 0, 0, 0, 5, -3, 0, 4, -1, 0, 0, -1},
+                           {0, 4, 2, 3, 3, 3, -6, -2, 1, -1, 1, 0});
+
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  std::vector<int32_t> expected_values = {13};
+  EXPECT_THAT(model.GetOutput<int32_t>(), ElementsAreArray(expected_values));
+}
+
 TEST(StablehloDotGeneralModelTest, DotGeneralFloat32) {
   TfLiteStablehloDotGeneralParams params = {
       {0},  // lhs_batching_dimensions;
@@ -111,6 +139,45 @@ TEST(StablehloDotGeneralModelTest, DotGeneralFloat32) {
   std::vector<float> expected_values = {1.32, 2.64, 3.96, 5.16,
                                         6.6,  7.2,  8.4,  9.6};
   EXPECT_THAT(model.GetOutput<float>(), Pointwise(FloatEq(), expected_values));
+}
+
+TEST(StablehloDotGeneralModelTest, DotGeneralFloat32_EmptyBatchingDims) {
+  TfLiteStablehloDotGeneralParams params = {
+      {},   // lhs_batching_dimensions;
+      0,    // num_lhs_batching_dimensions
+      {},   // rhs_batching_dimensions;
+      0,    // num_rhs_batching_dimensions
+      {1},  // lhs_contracting_dimensions;
+      1,    // num_lhs_contracting_dimensions
+      {0},  // rhs_contracting_dimensions;
+      1,    // num_rhs_contracting_dimensions
+      2,    // num_precision_configs
+      {tflite::StablehloPrecisionConfig::StablehloPrecisionConfig_DEFAULT,
+       tflite::StablehloPrecisionConfig::
+           StablehloPrecisionConfig_DEFAULT}  // precision config;
+  };
+  StablehloDotGeneralOpModel model({TensorType_FLOAT32, {4, 3}},
+                                   {TensorType_FLOAT32, {3, 6}},
+                                   {TensorType_FLOAT32, {}}, params);
+
+  model.SetInputs<float>(
+      {5.81311798, 2.08485532, 0.151162371, -1.21007407, -1.59476554,
+       0.846119463, -0.83784312, -0.416278511, 1.24929118, 3.46354723,
+       2.21915126, 3.81866336},
+      {-2.10215521, -1.803730, -7.83739519, 4.36787844, 1.4788357, 3.10357666,
+       -4.46420813, 0.879630148, -2.18081808, -1.95115197, -3.56435633,
+       -0.671983778, -2.76886797, -0.212248296, 2.77085519, -1.21441388,
+       -3.28464937, -4.60568237});
+
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  std::vector<float> expected_values = {
+      -21.9458523, -8.6834774,  -49.6875458, 21.1395512,  0.668963671,
+      15.9442625,  7.32033587,  0.600255728, 15.3061972,  -3.20136595,
+      1.11560607,  -6.58085871, 0.160507768, 0.87991172,  10.9359407,
+      -4.36453056, -3.85875082, -8.07441616, -27.7610416, -5.10577631,
+      -21.4037914, 6.1610136,   -15.3307991, -8.329400};
+  EXPECT_THAT(model.GetOutput<float>(),
+              Pointwise(FloatNear(1e-5), expected_values));
 }
 
 TEST(StablehloDotGeneralModelTest, DotGeneralFloat16) {
@@ -145,6 +212,51 @@ TEST(StablehloDotGeneralModelTest, DotGeneralFloat16) {
       Eigen::half(1.31934), Eigen::half(2.6386), Eigen::half(3.9609),
       Eigen::half(5.1601),  Eigen::half(6.6015), Eigen::half(7.2031),
       Eigen::half(8.3984),  Eigen::half(9.6015)};
+  EXPECT_THAT(model.GetOutput<Eigen::half>(),
+              Pointwise(FloatNear(1e-5), expected_values));
+}
+
+TEST(StablehloDotGeneralModelTest, DotGeneralFloat16_MultipleBatchingDims) {
+  TfLiteStablehloDotGeneralParams params = {
+      {0, 3},  // lhs_batching_dimensions;
+      2,       // num_lhs_batching_dimensions
+      {0, 3},  // rhs_batching_dimensions;
+      2,       // num_rhs_batching_dimensions
+      {2},     // lhs_contracting_dimensions;
+      1,       // num_lhs_contracting_dimensions
+      {2},     // rhs_contracting_dimensions;
+      1,       // num_rhs_contracting_dimensions
+      2,       // num_precision_configs
+      {tflite::StablehloPrecisionConfig::StablehloPrecisionConfig_DEFAULT,
+       tflite::StablehloPrecisionConfig::
+           StablehloPrecisionConfig_DEFAULT}  // precision config;
+  };
+  StablehloDotGeneralOpModel model({TensorType_FLOAT16, {2, 2, 2, 2}},
+                                   {TensorType_FLOAT16, {2, 2, 2, 2}},
+                                   {TensorType_FLOAT16, {}}, params);
+
+  std::initializer_list<Eigen::half> lhs_data{
+      Eigen::half(1.1),   Eigen::half(2.2),   Eigen::half(3.3),
+      Eigen::half(4.3),   Eigen::half(5.5),   Eigen::half(6.0),
+      Eigen::half(7.0),   Eigen::half(8.0),   Eigen::half(11.1),
+      Eigen::half(12.22), Eigen::half(33.33), Eigen::half(44.32),
+      Eigen::half(15.15), Eigen::half(6.6),   Eigen::half(7.3),
+      Eigen::half(8.1)};
+  std::initializer_list<Eigen::half> rhs_data{
+      Eigen::half(1.2), Eigen::half(0.0), Eigen::half(0.0), Eigen::half(1.2),
+      Eigen::half(1.2), Eigen::half(0.0), Eigen::half(0.0), Eigen::half(1.2),
+      Eigen::half(1.2), Eigen::half(0.0), Eigen::half(0.0), Eigen::half(1.2),
+      Eigen::half(1.2), Eigen::half(0.0), Eigen::half(0.0), Eigen::half(1.2)};
+  model.SetInputs<Eigen::half>(lhs_data, rhs_data);
+
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  std::initializer_list<Eigen::half> expected_values{
+      Eigen::half(1.319),  Eigen::half(1.319),  Eigen::half(6.6),
+      Eigen::half(6.6),    Eigen::half(5.16),   Eigen::half(5.16),
+      Eigen::half(9.6),    Eigen::half(9.6),    Eigen::half(13.32),
+      Eigen::half(13.32),  Eigen::half(18.18),  Eigen::half(18.18),
+      Eigen::half(53.184), Eigen::half(53.184), Eigen::half(9.7265),
+      Eigen::half(9.7265)};
   EXPECT_THAT(model.GetOutput<Eigen::half>(),
               Pointwise(FloatNear(1e-5), expected_values));
 }
@@ -184,6 +296,48 @@ TEST(StablehloDotGeneralModelTest, DotGeneralBFloat16) {
       Eigen::bfloat16(3.96875), Eigen::bfloat16(5.1875),
       Eigen::bfloat16(6.6250),  Eigen::bfloat16(7.21875),
       Eigen::bfloat16(8.4375),  Eigen::bfloat16(9.6250)};
+  EXPECT_THAT(model.GetOutput<Eigen::bfloat16>(),
+              Pointwise(FloatNear(1e-5), expected_values));
+}
+
+TEST(StablehloDotGeneralModelTest, DotGeneralBFloat16_MultipleContractingDims) {
+  TfLiteStablehloDotGeneralParams params = {
+      {0},     // lhs_batching_dimensions;
+      1,       // num_lhs_batching_dimensions
+      {0},     // rhs_batching_dimensions;
+      1,       // num_rhs_batching_dimensions
+      {2, 1},  // lhs_contracting_dimensions;
+      2,       // num_lhs_contracting_dimensions
+      {1, 2},  // rhs_contracting_dimensions;
+      2,       // num_rhs_contracting_dimensions
+      2,       // num_precision_configs
+      {tflite::StablehloPrecisionConfig::StablehloPrecisionConfig_DEFAULT,
+       tflite::StablehloPrecisionConfig::
+           StablehloPrecisionConfig_DEFAULT}  // precision config;
+  };
+  StablehloDotGeneralOpModel model({TensorType_BFLOAT16, {1, 3, 4}},
+                                   {TensorType_BFLOAT16, {1, 4, 3}},
+                                   {TensorType_BFLOAT16, {}}, params);
+
+  std::initializer_list<Eigen::bfloat16> lhs_data{
+      Eigen::bfloat16(4.968750e+00),  Eigen::bfloat16(-1.101560e+00),
+      Eigen::bfloat16(1.015630e+00),  Eigen::bfloat16(4.812500e+00),
+      Eigen::bfloat16(-3.398440e-01), Eigen::bfloat16(2.484380e+00),
+      Eigen::bfloat16(-5.187500e+00), Eigen::bfloat16(-1.109380e+00),
+      Eigen::bfloat16(-1.328130e+00), Eigen::bfloat16(3.312500e+00),
+      Eigen::bfloat16(-4.937500e+00), Eigen::bfloat16(-4.281250e+00)};
+  std::initializer_list<Eigen::bfloat16> rhs_data{
+      Eigen::bfloat16(-1.164060e+00), Eigen::bfloat16(1.437500e+00),
+      Eigen::bfloat16(-4.638670e-02), Eigen::bfloat16(-1.945310e+00),
+      Eigen::bfloat16(-5.187500e+00), Eigen::bfloat16(-1.414060e+00),
+      Eigen::bfloat16(-2.031250e+00), Eigen::bfloat16(-3.656250e+00),
+      Eigen::bfloat16(-1.738280e-01), Eigen::bfloat16(4.902340e-01),
+      Eigen::bfloat16(-5.968750e+00), Eigen::bfloat16(-3.671880e+00)};
+  model.SetInputs<Eigen::bfloat16>(lhs_data, rhs_data);
+
+  ASSERT_EQ(model.Invoke(), kTfLiteOk);
+  std::initializer_list<Eigen::bfloat16> expected_values = {
+      Eigen::bfloat16(2.087500e+01)};
   EXPECT_THAT(model.GetOutput<Eigen::bfloat16>(),
               Pointwise(FloatNear(1e-5), expected_values));
 }
